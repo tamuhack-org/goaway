2b0868a7dca22e11601b05f42625ab4f7c436785
NitroGuy10 <naviyer10@gmail.com>
2024-08-15 21:25:25 -0700

    Redirect to the specified route

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("*", (req, res) => {
     const redirect = REDIRECTS[host];
     if (redirect) {
         const [url, status] = redirect;
-        return res.redirect(status, url);
+        return res.redirect(status, url + req.url);
     }
     res.send("Looks like you're lost, young traveler. It's ok, we all get lost sometimes. Come, stay a while. Enjoy some time in this cozy little corner of the internet. It's a scary world out there, but you're safe here. Whenever you're ready, you can resume your journey. But for now, just relax. You're home. You're safe. You're loved. Cheers, friend. 🌌");
 });
